9f0c21836e6bbcaecc2b38141fc740d3a42e130d
EmperorXSama <5alidghoddane@protonmail.com>
2025-10-06 09:08:12 +0100

    Add missing files

---
+++ Reporting.lib/Models/DTO/FailedEmailDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Reporting.lib.Models.DTO;
+
+public class FailedEmailDto
+{
+    public int EmailId { get; set; }
+    public string FailureReason { get; set; }
+}
+public class RetrieveFailedEmailDto
+{
+    public int Id { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
+    public string ProxyIp { get; set; }
+    
+    public int Port { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string ProxyPassword { get; set; }
+}
+public class EmailMetadata
+{
+    public int EmailAccountId { get; set; } // Foreign key to EmailAccount table
+    public string MailId { get; set; }
+    public string YmreqId { get; set; }
+    public string Wssid { get; set; }
+    public string Cookie { get; set; }
+}
+
---
+++ Reporting.lib/Models/DTO/ProxyUpdateRegion.cs
@@ -0,0 +1,12 @@
+﻿namespace Reporting.lib.Models.DTO;
+
+public class ProxyUpdateRegion
+{
+    [JsonPropertyName("proxyId")]
+    public int ProxyId { get; set; }
+
+    public string? YahooConnectivity { get; set; }
+    public string? Region { get; set; }
+    [JsonPropertyName("availability")] public bool Availability { get; set; }
+}
+ 
